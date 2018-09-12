c19f0b15e94735419e1afd552379e4e513c660bf
matsakelarsson <m.larsson88@gmail.com>
2018-09-13 00:41:13 +0200

    Update uppgift.py

--- uppgift.py
+++ uppgift.py
@@ -8,14 +8,17 @@ canvas.grid()
 #--------------------------- Magiska funktioner -----------------------------
 
 def cirkel(x, y,r, **kwargs):
-    return create_oval(x-r, y-r, x+r, y+r, **kwargs)
+    return canvas.create_oval(x-r, y-r, x+r, y+r, **kwargs)
 
 
 def cirkel_del(x, y, r, **kwargs):
     if "start" in kwargs and "end" in kwargs:
         kwargs["extent"] = kwargs["end"] - kwargs["start"]
         del kwargs["end"]
-    return create_arc(x-r, y-r, x+r, y+r, **kwargs)
+    return canvas.create_arc(x-r, y-r, x+r, y+r, **kwargs)
+
+def rektangel(x1, y1, x2, y2):
+    return canvas.create_rectangle(x1, y1, x2, y2)
 
 
 #--------------------------- Jobba här ---------------------------------
