f5ac21e5b6e76ac713a16304717d9f67d984ab74
Mats Larsson <m.larsson88@gmail.com>
2018-11-07 12:34:07 +0100

    single outline argument

--- twicon.py
+++ twicon.py
@@ -9,38 +9,39 @@ r = 40
 
 gs = 1.618034
 
+kontur = ""
 
 #---------------------- Uppgiften ------------------------
 
 # STORA BLÅ CIRKELN
 
-paint.circle_arc(r*5, r*5, r*gs*2, fill="blue", outline="", start=200, end=360)
+paint.circle_arc(r*5, r*5, r*gs*2, fill="blue", outline=kontur, start=200, end=360)
 
 
 # Fyll i x = 180
 # Fyll i y = 160
 
-#paint.circle(, , r, fill="blue", outline="")
+#paint.circle(, , r, fill="blue", outline=kontur)
 
 # VINGARNA
 
-paint.circle(r*3.5, r*6, r*gs, fill="white", outline="")
+paint.circle(r*3.5, r*6, r*gs, fill="white", outline=kontur)
 
 # Fyll i x = 190
 # Fyll i y = 220
 # Multiplicera r med gs
 
-#paint.circle(, , r, fill="blue", outline="")
+#paint.circle(, , r, fill="blue", outline=kontur)
 
-paint.circle(r*3.75, r*5, r*gs, fill="white", outline="")
-paint.circle(r*4.75, r*5, r*gs, fill="blue", outline="")
-paint.circle(r*3.75, r*4.25, r*gs, fill="white", outline="")
+paint.circle(r*3.75, r*5, r*gs, fill="white", outline=kontur)
+paint.circle(r*4.75, r*5, r*gs, fill="blue", outline=kontur)
+paint.circle(r*3.75, r*4.25, r*gs, fill="white", outline=kontur)
 
 # Fyll i x = 200
 # Fyll i y = 180
 # Multiplicera r med gs
 
-#paint.circle(, , r, fill="blue", outline="")
+#paint.circle(, , r, fill="blue", outline=kontur)
 
 # STORA VITA CIRKEL
 
@@ -48,20 +49,20 @@ paint.circle(r*3.75, r*4.25, r*gs, fill="white", outline="")
 # Fyll i y = 90
 # Multiplicera r med gs och 2
 
-#paint.circle(, , r, fill="white", outline="")
+#paint.circle(, , r, fill="white", outline=kontur)
 
 # NÄBBEN
 
-paint.circle_arc(r*7.75, r*4.75, r, fill="blue", outline="", start=200, end=360)
+paint.circle_arc(r*7.75, r*4.75, r, fill="blue", outline=kontur, start=200, end=360)
 
-paint.circle(r*8.15, r*4.25, r, fill="white", outline="")
+paint.circle(r*8.15, r*4.25, r, fill="white", outline=kontur)
 
-paint.circle_arc(r*7.5, r*4.35, r, fill="blue", outline="", start=200, end=360)
+paint.circle_arc(r*7.5, r*4.35, r, fill="blue", outline=kontur, start=200, end=360)
 
 # Fyll i x = 314
 # Fyll i y = 150
 
-#paint.circle(, , r, fill="white", outline="")
+#paint.circle(, , r, fill="white", outline=kontur)
 
 
 # HUVUDET
@@ -69,7 +70,7 @@ paint.circle_arc(r*7.5, r*4.35, r, fill="blue", outline="", start=200, end=360)
 # Fyll i x = 280
 # Fyll i y = 210
 
-#paint.circle(, , r, fill="blue", outline="")
+#paint.circle(, , r, fill="blue", outline=kontur)
 
 
 #-------------------------------------------------------------------------------
